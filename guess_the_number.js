c142f89efc7194b25fd7b334e58921619759c204
Peter <monroepe@gmail.com>
2014-09-16 15:40:10 -0400

    Validates

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,4 +1,6 @@
 var hiddenNumber = Math.floor((Math.random() * 100) + 1);
+var MAX = 100;
+var guesses = 0;
 console.log(hiddenNumber);
 
 $('#play_again').hide()
@@ -10,19 +12,25 @@ $(document).ready(function(){
 
     var guess = $('#guess').val()
 
+    if (isNaN(guess)){
+      alert('Invalid input, must enter a number between 0 and ' + MAX);
+    } else{
+
     if (guess > hiddenNumber){
-      $('#special_text').html(guess + ' is too high. Try again...');
+      ++guesses;
+      $('#special_text').html(guess + ' is too high. Guesses: ' + guesses + ' Try again...');
     } else if (guess < hiddenNumber){
-      $('#special_text').html(guess + ' is too low. Try again...');
+      ++guesses;
+      $('#special_text').html(guess + ' is too low. Guesses: ' + guesses + '  Try again...');
     } else {
-      $('#special_text').html(guess + ' is just right. You win');
+      $('#special_text').html(guess + ' is just right. You won in ' + guesses + ' guesses.');
       $('#play_again').show()
     };
 
     $('#play_again').on('click', function(event){
       document.location.reload();
     });
-
+  };
 
   });
 
