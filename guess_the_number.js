a1ae8d50aea2d8016dcdeda03e84edd67520ff9c
Peter <monroepe@gmail.com>
2014-09-16 13:32:26 -0400

    Messed up all the code

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,13 +1,44 @@
+var MAX = 100;
+var guesses = 0;
+var playAgain = true
+
+function range(){
+if (guess < number){
+  alert(guess + ' is too low. Try again...');
+} else {
+  alert(guess + ' is too high. Try again...');
+}
+};
+
+function validInput(input){
+  typeof input === 'integer' && input > 1 && input < MAX
+};
+
+while (playAgain){
 alert('Welcome to guess the number!');
 
-var name = prompt('What is your name?');
-alert('Hello ' + name);
+var number = Math.floor((Math.random() * MAX) + 1);
+alert(number);
 
-var number = Math.floor((Math.random() * 100) + 1);
-var guess = prompt('Guess a number between 1-100!');
+var guess = prompt('Guess a number between 1-' + MAX);
 
-if (number === guess){
-  alert('You won');
-} else {
-  alert('You lose');
-}
+while (validInput(guess)){
+  guess = prompt('Guess a number between 1-' + MAX);
+};
+
+while (number != guess){
+  if (number != guess){
+    range();
+    guess = prompt('Guess a number between 1-' + MAX);
+    guesses++;
+  };
+};
+
+alert('You won');
+
+var again = prompt('Do you want to play again? (y/n)');
+if (again == 'n'){
+  playAgain = false;
+};
+};
+
